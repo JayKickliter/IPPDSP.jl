5da6d5f4af9ae29d639ac7d19069f3b4d6fbe851
Jay Kickliter <jay.kickliter@gmail.com>
2014-07-07 09:12:36 -0600

    Base code copied from IPPMath.jl

--- src/IPPDSP.jl
+++ src/IPPDSP.jl
@@ -1,5 +1,21 @@
 module IPPDSP
 
-# package code goes here
+    using IPPCore
+    import IPPCore: IppInt, ippint
+
+    # common
+
+    macro ippscall(ippf, argtypes, args...)
+        quote
+            ret = ccall(($ippf, "libipps"), IppStatus,
+                $argtypes, $(args...))
+            ret == 0 || error(ippstatus_string(ret))
+        end
+    end
+
+    # source files
+
+    include("Windows.jl")
+    include("Filtering.jl")
 
 end # module
