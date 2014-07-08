cd959f24c8c05416a3c8c599d5b27d449e5ca179
Jay Kickliter <jay.kickliter@gmail.com>
2014-07-07 22:20:48 -0600

    Formatting

--- src/IPPDSP.jl
+++ src/IPPDSP.jl
@@ -1,21 +1,22 @@
 module IPPDSP
 
-    using IPPCore
-    import IPPCore: IppInt, ippint
+using IPPCore
+import IPPCore: IppInt, ippint
 
-    # common
 
-    macro ippscall(ippf, argtypes, args...)
-        quote
-            ret = ccall(($ippf, "libipps"), IppStatus,
-                $argtypes, $(args...))
-            ret == 0 || error(ippstatus_string(ret))
-        end
+# common
+
+macro ippscall(ippf, argtypes, args...)
+    quote
+        ret = ccall(($ippf, "libipps"), IppStatus,
+            $argtypes, $(args...))
+        ret == 0 || error(ippstatus_string(ret))
     end
+end
 
-    # source files
+# source files
 
-    include("Windows.jl")
-    include("Filtering.jl")
+include("Windows.jl")
+include("Filtering.jl")
 
 end # module
