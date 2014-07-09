7673291e7a47b2d219a93760fe7001d9148d1fe0
Jay Kickliter <jay@kickliter.com>
2014-07-09 17:18:49 -0600

    Created new IPP type aliases

--- src/IPPDSP.jl
+++ src/IPPDSP.jl
@@ -6,6 +6,17 @@ import IPPCore: IppInt, ippint
 
 # common
 
+typealias IPP16s    Int16
+typealias IPP32s    Int32
+typealias IPP16f    Float16
+typealias IPP32f    Float32
+typealias IPP64f    Float64
+typealias IPP16sc   Complex{Int16}
+typealias IPP32sc   Complex{Int32}
+typealias IPP16fc   Complex{Float16}
+typealias IPP32fc   Complex{Float32}
+typealias IPP64fc   Complex{Float64}
+
 macro ippscall(ippf, argtypes, args...)
     quote
         ret = ccall(($ippf, "libipps"), IppStatus,
