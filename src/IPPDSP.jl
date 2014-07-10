9fe0db66c4f4a332eeafcec2d18fb825b534fd1a
Jay Kickliter <jay.kickliter@gmail.com>
2014-07-09 20:10:55 -0600

    Added IPP type convertion functions.

--- src/IPPDSP.jl
+++ src/IPPDSP.jl
@@ -17,6 +17,22 @@ typealias IPP16fc   Complex{Float16}
 typealias IPP32fc   Complex{Float32}
 typealias IPP64fc   Complex{Float64}
 
+ipp16s( x )  = convert( IPP16s, x ) 
+ipp32s( x )  = convert( IPP32s, x ) 
+ipp16f( x )  = convert( IPP16f, x ) 
+ipp32f( x )  = convert( IPP32f, x ) 
+ipp64f( x )  = convert( IPP64f, x ) 
+ipp16sc( x ) = convert( IPP16sc, x )
+ipp32sc( x ) = convert( IPP32sc, x )
+ipp16fc( x ) = convert( IPP16fc, x )
+ipp32fc( x ) = convert( IPP32fc, x )
+ipp64fc( x ) = convert( IPP64fc, x )
+
+
+
+
+
+
 macro ippscall(ippf, argtypes, args...)
     quote
         ret = ccall(($ippf, "libipps"), IppStatus,
