d74bd32a8cffa91c14328a11682b3f9e78d0bfd3
Jay Kickliter <jay.kickliter@gmail.com>
2014-07-13 06:55:26 -0600

    Added test for conv and xcorr

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,27 @@
-using IPPDSP
+using IPPCore
+import IPPDSP
 using Base.Test
 
 # write your own tests here
-@test 1 == 1
+
+for T = IPPDSP.conv_types
+    a = convert(Vector{eval(T)}, [ x = 1:10 ] )
+    b = convert(Vector{eval(T)}, [ x = 11:21 ] )
+    reference = conv( a, b )
+    IPPResult = IPPDSP.conv( a, b )
+    @test length( reference ) == length( IPPResult )
+    for i = 1:length( reference )
+        @test isapprox( reference[i], IPPResult[i] )
+    end
+end
+
+for T = IPPDSP.xcorr_types
+    a = convert(Vector{eval(T)}, [ x = 1:10 ] )
+    b = convert(Vector{eval(T)}, [ x = 1:10 ] )
+    reference = xcorr( a, b )
+    IPPResult = IPPDSP.xcorr( a, b, lowlag = -9 )
+    @test length( reference ) == length( IPPResult )
+    for i = 1:length( reference )
+        @test isapprox( reference[i], IPPResult[i] )
+    end
+end
